610e16aacc5bc1ba38ac50661193b3832c13e6e8
G-NooN Jo <jjjchoon@nate.com>
2026-06-03 23:12:35 +0900

    weather-17 chore/style: 섹션 간격 및 너비 설정

--- apps/web/features/home/homepage-container.tsx
+++ apps/web/features/home/homepage-container.tsx
@@ -4,13 +4,13 @@ import HourlyWeatherSection from './components/hourly-weather.section'
 
 function HomepageContainer() {
 	return (
-		<div className="flex">
-			<div className="w-2/3">
+		<div className="flex gap-10">
+			<div className="flex w-2/3 flex-col gap-6">
 				<CurrentWeatherSection />
 				<HourlyWeatherSection />
 				<DailyWeatherSection />
 			</div>
-			<div className="w-1/3">
+			<div className="flex w-1/3 flex-col gap-6">
 				<section>검색</section>
 				<section>자외선 정보</section>
 				<section>일출/일몰</section>
