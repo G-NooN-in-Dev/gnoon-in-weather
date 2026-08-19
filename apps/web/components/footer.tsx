183d0ad10365b28163fe6f449ce44ecee9da0aa9
G-NooN Jo <jjjchoon@nate.com>
2026-08-19 19:01:28 +0900

    weather-38 feat: 공항 > 지도, 야구장 > 지도 페이지 - 푸터 제거

--- apps/web/components/footer.tsx
+++ apps/web/components/footer.tsx
@@ -1,4 +1,14 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+import { hideFooter } from '@/lib/layout/hide-footer'
+
 function Footer() {
+	const pathname = usePathname()
+
+	if (hideFooter(pathname)) return null
+
 	return <footer>Footer 추가 예정</footer>
 }
 
