ff67d194163d46a50465164ad711c245ebf73fe3
G-NooN Jo <jjjchoon@nate.com>
2026-09-02 21:44:45 +0900

    weather-42 feat: 푸터 추가

--- apps/web/components/footer.tsx
+++ apps/web/components/footer.tsx
@@ -9,7 +9,11 @@ function Footer() {
 
 	if (hideFooter(pathname)) return null
 
-	return <footer>Footer 추가 예정</footer>
+	return (
+		<footer className="shadow-soft flex h-14 items-center justify-center bg-white">
+			<p className="text-grayscale-500 font-medium">2026 G-NooN All Rights Reserved.</p>
+		</footer>
+	)
 }
 
 export default Footer
