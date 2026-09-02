794dbd236aa47acacc22fbec724ef5621f6469ef
G-NooN Jo <jjjchoon@nate.com>
2026-09-02 16:57:00 +0900

    feat: 로고 및 favicon 변경

--- apps/web/components/logo.tsx
+++ apps/web/components/logo.tsx
@@ -4,7 +4,7 @@ function Logo() {
 	return (
 		<div className="flex items-baseline gap-3">
 			<Link href="/" className="inline-block" aria-label="G-NooN 메인으로 이동">
-				<span className="inline-block translate-y-0.5 bg-linear-to-r from-(--color-pastel-red) via-(--color-pastel-green) to-(--color-pastel-blue-700) bg-clip-text text-3xl leading-none font-bold text-transparent">
+				<span className="from-pastel-red-700 via-pastel-green-700 to-pastel-blue-800 inline-block translate-y-0.5 bg-linear-to-r bg-clip-text text-3xl leading-none font-bold text-transparent">
 					G
 				</span>
 			</Link>
